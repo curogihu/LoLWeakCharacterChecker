ab8302f19cc5e55af035ab24148a3a0a092609ea
curogihu <curoasg@gmail.com>
2018-09-24 12:54:31 +0900

    フロント

--- resources/views/template/basic.blade.php
+++ resources/views/template/basic.blade.php
@@ -22,12 +22,8 @@
 
       <section class="jumbotron text-center">
         <div class="container">
-          <h1 class="jumbotron-heading">Album example</h1>
-          <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
-          <p>
-            <a href="#" class="btn btn-primary my-2">Main call to action</a>
-            <a href="#" class="btn btn-secondary my-2">Secondary action</a>
-          </p>
+          <h1 class="jumbotron-heading">Weak Character Checker</h1>
+          <p class="lead text-muted">対象プレイヤーの苦手なチャンピオンを検索するサイトです。</p>
         </div>
       </section>
 
@@ -39,17 +35,18 @@
               <div class="card mb-4 shadow-sm">
                 <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
                 <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                      <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
+                  <p class="card-text">対象サモナーネーム</p>
+                  <input style="width:100%;" type="text" name="champion1">
+                  <p class="card-text">対象チャンピオン</p>
+                  <input style="width:100%;" type="text" name="champion1">
+                  <input style="width:100%;" type="text" name="champion2">
+                  <input style="width:100%;" type="text" name="champion3">
+                  <input style="width:100%;" type="text" name="champion4">
+                  <input style="width:100%;" type="text" name="champion5">
                 </div>
               </div>
             </div>
+          </div>
             <div class="col-md-4">
               <div class="card mb-4 shadow-sm">
                 <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
